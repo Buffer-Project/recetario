12171e47574c9e09585532840c5c9ea6b8b5175e
Tuckson <franrache@gmail.com>
2022-11-25 16:47:49 -0300

    cambios basicos al sitio

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -2,8 +2,18 @@ export default function Header() {
 
     return (
        <header>
-            <nav>
-               Barra de navegacion con separaciones
+         <div id="div_logo">
+            <a href="App.js">
+               <img id="icono_inicio" src="https://png.pngtree.com/png-vector/20210303/ourmid/pngtree-cat-paw-shape-cartoon-white-chef-hat-png-image_2987315.jpg"></img>
+            </a>
+         </div>
+            <nav id="nav_bar">
+               <ul className="navBar">
+                  <li><a href=""></a>Platos </li>
+                  <li><a href=""></a>Bebidas </li>
+                  <li><a href=""></a>Postres </li>
+                  <li><a href=""></a>Publica tu receta </li>                  
+               </ul>
             </nav>
        </header>
         
