60a265e5f5ab48fb9f4e8b67ec55aa7d020b880f
Tuckson <franrache@gmail.com>
2022-11-28 15:12:10 -0300

    inicio de boton login-register

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,21 +1,24 @@
 export default function Header() {
 
-    return (
-       <header>
+   return (
+      <header>
          <div id="div_logo">
             <a href="App.js">
                <img id="icono_inicio" src="https://png.pngtree.com/png-vector/20210303/ourmid/pngtree-cat-paw-shape-cartoon-white-chef-hat-png-image_2987315.jpg"></img>
             </a>
          </div>
-            <nav id="nav_bar">
-               <ul className="navBar">
-                  <li><a href=""></a>Platos </li>
-                  <li><a href=""></a>Bebidas </li>
-                  <li><a href=""></a>Postres </li>
-                  <li><a href=""></a>Publica tu receta </li>                  
-               </ul>
-            </nav>
-       </header>
-        
-    )
+         <nav id="nav_bar">
+            <ul className="navBar">
+               <li><a href=""></a>Platos </li>
+               <li><a href=""></a>Bebidas </li>
+               <li><a href=""></a>Postres </li>
+               <li><a href=""></a>Publica tu receta </li>
+            </ul>
+            <div id="boton_login-register">
+               Login / Register
+            </div>
+         </nav>
+      </header>
+
+   )
 }
