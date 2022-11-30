c584b7396527d79c4751028a7d320ee95da5ec9d
Tuckson <franrache@gmail.com>
2022-11-30 18:20:04 -0300

    redes sociales en banner

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,9 +1,22 @@
 export default function Main() {
 
     return (
-       <main>
-        
-       </main>
-        
+        <main>
+            <div id="carrouselDeRecetas">
+                <div>
+                    <h3 id="autorReceta">Autor de la receta</h3>
+                </div>
+                <div id="imagenesDeRecetas">
+
+                </div>
+                <div id="nombreDeReceta">
+                        <p>
+                            Nombre de la receta
+                        </p>
+                </div>
+            </div>
+
+        </main>
+
     )
 }
