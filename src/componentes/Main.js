d602066fc5f6cf562d71e4080ae9f8179a76902a
Tuckson <franrache@gmail.com>
2022-12-01 15:14:34 -0300

    redes sociales mejorado y suscripcion a newletter

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -2,7 +2,8 @@ export default function Main() {
 
     return (
         <main>
-            <div id="carrouselDeRecetas">
+            <input type="text" class="nav-search-input" aria-label="Ingresá lo que quieras encontrar" name="as_word" placeholder="Buscar productos, marcas y más…" maxlength="120" autofocus="" autocapitalize="none" autocorrect="off" spellcheck="false" autocomplete="off" tabindex="2" value="" aria-activedescendant="" aria-controls="cb1-list" aria-autocomplete="list" id="cb1-edit" aria-expanded="false" role="combobox"></input>
+            <section id="carrouselDeRecetas">
                 <div>
                     <h3 id="autorReceta">Autor de la receta</h3>
                 </div>
@@ -14,7 +15,7 @@ export default function Main() {
                             Nombre de la receta
                         </p>
                 </div>
-            </div>
+            </section>
 
         </main>
 
