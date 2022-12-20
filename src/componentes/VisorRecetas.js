1496e3e3783f00a4259e086dc8d5930c5b2b6054
Franco R <franrache@gmail.com>
2022-12-20 12:22:44 -0300

    visorRecetas

--- src/componentes/VisorRecetas.js
+++ src/componentes/VisorRecetas.js
@@ -1,10 +1,14 @@
 import recetas from "./Recetas.json"
 export default function VisorRecetas() {
 
+    const receta = recetas[0]
+    console.log(receta)
+    console.log(recetas)
+
     return (
 
         <div>
-
+            {receta.name} hola
         </div>
 
 
