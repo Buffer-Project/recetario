1d6a378a7ca519edbeb1e16ebbfc20e6e5cecd28
Tuckson <franrache@gmail.com>
2022-12-21 09:41:17 -0300

    main.js

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -6,34 +6,10 @@ import VisorRecetas from "./VisorRecetas"
 export default function Main () {
     return (
         <main>
-<<<<<<< Updated upstream
         <Routes>
             <Route exact path={"/"} element={<Landing/>} />
             <Route path={"/recetas"} element={<ListaRecetas/>} />
             <Route exact path={"/receta/:id"} element={<VisorRecetas/>} />
-=======
-            <div id="div-searchbar">
-                <input id="searchbar" type="search" placeholder="Buscar recetas, ingredientes, y más..." ></input>
-                <div id="div-icono-lupa">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrBPjW-HJAeJyvcXBJcZ-VspgyqXvfs-Nd4Kz3wug04w&s" alt="lupa"></img>
-                </div>
-            </div>
-            
-            <section id="carrouselDeRecetas">
-                <p>aca iria el carrousel de imagenes de recetas</p>
-                <div>
-                    <h3 className="autorReceta">Autor de la receta</h3>
-                </div>
-                <div className="imagenesDeRecetas">
-
-                </div>
-                <div className="nombreDeReceta">
-                    <p>
-                        Nombre de la receta
-                    </p>
-                </div>
-            </section>
->>>>>>> Stashed changes
 
         </Routes>
         </main>
