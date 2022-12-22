f6744f0def7809e155dbb7a543b19456173be66c
Tuckson <franrache@gmail.com>
2022-12-22 13:14:29 -0300

    terminando componente visor y rating en estrellas

--- src/componentes/VisorRecetas.js
+++ src/componentes/VisorRecetas.js
@@ -1,6 +1,8 @@
+import { Rating } from 'primereact/rating';
+import { useState } from 'react';
 import recetas from "./Recetas.json"
 export default function VisorRecetas() {
-
+    const [value, setValue] = useState("")
     const mostrarIngredientes = () => {
 
         return (
@@ -8,6 +10,17 @@ export default function VisorRecetas() {
                 <li>{item.cantidad} de {item.ingrediente}</li>
 
             ))}</ul>
+        )
+
+    }
+
+    const mostrarPreparacion = () => {
+
+        return (
+            <ol>{recetas[0].preparacion.map(item => (
+                <li>{item}</li>
+
+            ))}</ol>
         )
 
     }
@@ -25,12 +38,24 @@ export default function VisorRecetas() {
             <div className="imagen-receta">
                 <img src={recetas[0].imagenReceta} alt="imagen-receta"></img>
             </div>
-            
+
             <div className="lista-ingredientes">
                 <p id="titulo-ingredientes"><b>Ingredientes:</b></p>
                 {mostrarIngredientes()}
             </div>
+            <div className="preparacion">
+                <p id="titulo-preparacion"><b>Preparacion</b></p>
+                {mostrarPreparacion()}
+            </div>
+            <div className='rating'>
+                <p><b>Dejanos tu opinion!</b></p>
+                <Rating value={value} onChange={(e) => setValue(e.value)} stars={5} cancel={false} />
+            </div>
         </div>
+
+
+
+
 
 
 
