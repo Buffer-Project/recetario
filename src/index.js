36b3abfc7374dcd6fcbcf44f73e5b99714f3cff9
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-03-29 22:50:08 -0300

    style: add primeflex.css

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
+import "primeflex/primeflex.css"
 import "primeicons/primeicons.css";
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
